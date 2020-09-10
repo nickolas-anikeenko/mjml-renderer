d81232231bda68a75f0ceef2d84e587f5489f9a9
timur_gilauri <timur_gilauri@epam.com>
2020-09-10 16:12:33 +0300

    compile on change

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import Handlebars from "handlebars";
 
 import {existsSync, mkdirSync, readdirSync, readFileSync, writeFileSync} from "fs";
-import {extname, join} from "path";
+import {extname, join, dirname, sep} from "path";
 import * as chokidar from "chokidar";
 import mjml from "mjml";
 
@@ -9,11 +9,12 @@ import mjml from "mjml";
 const layoutsPath = join(__dirname, './layouts')
 const compiledPath = join(__dirname, './compiled')
 
-const dirsToRecompileAlways = ['style']
+const dirsToTriggerRecompileAll = ['style']
+const dirsToExcludeCompile = ['style']
 
-const compile = (dirname: string) => {
-    const layoutPath = join(layoutsPath, dirname, 'layout.mjml')
-    const dataPath = join(layoutsPath, dirname, 'data.json')
+const compileTemplate = (pathToLayoutDir: string) => {
+    const layoutPath = join(pathToLayoutDir, 'layout.mjml')
+    const dataPath = join(pathToLayoutDir, 'data.json')
 
     const layout = readFileSync(layoutPath).toString()
     const data = existsSync(dataPath) ? JSON.parse(readFileSync(dataPath).toString()) : {}
@@ -21,7 +22,7 @@ const compile = (dirname: string) => {
     return template(data)
 }
 
-const writeCompiled = (dirToSave, content) => {
+const writeCompiledTemplate = (dirToSave, content) => {
     const filename = 'layout.html'
     try {
         mkdirSync(dirToSave)
@@ -31,6 +32,30 @@ const writeCompiled = (dirToSave, content) => {
     writeFileSync(join(dirToSave, filename), content)
 }
 
+const compileOne = (dirName) => {
+    console.log(`compile ${dirName}`)
+    const template = compileTemplate(join(layoutsPath, dirName))
+    writeCompiledTemplate(join(compiledPath, dirName), template)
+}
+
+const compileAll = () => {
+    return readdirSync(layoutsPath)
+        .filter(dirName => !dirsToExcludeCompile.includes(dirName))
+        .forEach(compileOne)
+}
+
+
 chokidar.watch(layoutsPath)
-    .on('change', (path: string) => console.log(path))
+    .on('change', async (path: string) => {
+        const dirName = dirname(path).split(sep).pop()
+        console.log(`content in ${dirName} dir has been changed`)
+        if (dirsToTriggerRecompileAll.includes(dirName)) {
+            console.log('compile all')
+            return compileAll()
+        } else {
+            console.log('compile one')
+            return compileOne(dirName)
+        }
+
+    })
 
